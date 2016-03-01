556d7d6f1cc3aea746fe5dcb971a553b2a807580
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-03-01 07:33:32 +0100

    Add rember-f?

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Little schemer and seasoned and reasoned schemer  in Clojure.
 
 
 ## Some Useful Resources 
-
+* [The Little Schemer in Clojure – Chapter 9 – Lambda The Ultimate
+](http://juliangamble.com/blog/2012/09/15/the-little-schemer-in-clojure-chapter-9-lambda-the-ultimate-deriving-the-y-combinator/)
--- the-little-schemer/littleclj.clj
+++ the-little-schemer/littleclj.clj
@@ -2,6 +2,7 @@
 
 ;; Lambda the utlimate
 
+;; 1st version
 (def rember-f?
   (fn [test? a l]
     (cond
@@ -12,3 +13,26 @@
 
 
 (println (rember-f? = '(pop corn) '(lemonade (pop corn) and (cake))))
+
+;; 2nd version: removing the second condition
+(def rember-f2?
+  (fn [test? a l]
+    (cond
+      (nil? l) '()
+      (test? (first l) a) (rest l)
+      true (cons (first l) (rember-f? test? a (rest l))))))
+
+(println (rember-f2? = '(pop corn) '(lemonade (pop corn) and (cake))))
+
+;; 3d version
+(def rember-f3?
+  (fn [test? a l]
+      (nil? l) '()
+      (test? (first l) a) (rest l)
+      (cons (first l) (rember-f? test? a (rest l)))))
+
+(println (rember-f3? = '(pop corn) '(lemonade (pop corn) and (cake))))
+
+(println (rember-f2? = '(a) ())) ;; SO error!
+
+  ;; TODO: let's do rember-f in core.logic with pattern matching ? 
