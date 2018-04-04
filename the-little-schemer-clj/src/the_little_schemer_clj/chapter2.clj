794237938ee32fe2b1e80303b527794fd8be5ab3
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-04 11:12:23 +0100

    Chapter 2

--- the-little-schemer-clj/src/the_little_schemer_clj/chapter2.clj
+++ the-little-schemer-clj/src/the_little_schemer_clj/chapter2.clj
@@ -1,8 +1,19 @@
 (ns the-little-schemer.chapter2)
 
+(def atom?
+  (complement coll?))
+
 (def lat?
   (fn [l]
     (cond
       (empty? l) true
       (atom? (first l)) (lat? (rest l))
       :else false)))
+
+(defn member?
+  [a lat]
+  (cond
+    (empty? lat) false
+    :else (or (= (first lat) a)
+              (recur a (rest lat)))))
+
