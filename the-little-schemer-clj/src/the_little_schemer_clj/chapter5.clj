246c34031715d643aa2fc369afcd384becbb8c0d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-06 23:20:55 +0100

    implementation of insetR*

--- the-little-schemer-clj/src/the_little_schemer_clj/chapter5.clj
+++ the-little-schemer-clj/src/the_little_schemer_clj/chapter5.clj
@@ -1,27 +1,39 @@
 (ns the-little-schemer-clj.chapter5)
 
 (defn rember*
-  [a lat]
+  [a l]
   (cond
-    (empty? lat) lat
-    (= a (first lat)) (rember* a (rest lat))
+    (empty? l) l
+    (= a (first l)) (rember* a (rest l))
     :else
     (cond
-      (list? (first lat)) (cons (rember* a (first lat)) (rember* a (rest lat)))
-      :else (cons (first lat) (rember* a (rest lat))))))
+      (list? (first l)) (cons (rember* a (first l)) (rember* a (rest l)))
+      :else (cons (first l) (rember* a (rest l))))))
 
 (defn rember2*
-  [a lat]
+  [a l]
     (cond
-      (empty? lat) ()
-      (not (list? (first lat)))
+      (empty? l) ()
+      (not (list? (first l)))
             (cond
-              (= (first lat) a) (rember* a (rest lat))
-              :else (cons (first lat) (rember* a (rest lat))))
-      :else (cons (rember* a (first lat)) (rember* a (rest lat)))))
+              (= (first l) a) (rember* a (rest l))
+              :else (cons (first l) (rember* a (rest l))))
+      :else (cons (rember* a (first l)) (rember* a (rest l)))))
 
 ;(rember* 1 '(1 (2 1) 3))
 ;(rember* "cup" '(("coffee") "cup" (("tea") "cup") ("and" ("hick")) "cup"))
 
-(rember2* 1 '(1 (2 1) 3))
-(rember2* "cup" '(("coffee") "cup" (("tea") "cup") ("and" ("hick")) "cup"))
+;(rember2* 1 '(1 (2 1) 3))
+;(rember2* "cup" '(("coffee") "cup" (("tea") "cup") ("and" ("hick")) "cup"))
+
+(defn insertR*
+  [new old l]
+  (cond
+    (empty? l) l
+    (= old (first l)) (cons old (cons new (insertR* new old (rest l))))
+    :else
+    (cond
+      (list? (first l)) (cons (insertR* new old (first l)) (insertR* new old (rest l)))
+      :else (cons (first l) (insertR* new old (rest l))))))
+
+;(insertR* 1 2 '(2 (3 2) (2 4)))
