75413be7815240361ebb22d8e7d4f8c461f11899
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-08 21:34:49 +0100

    Rest of chapter 9 exercices.

--- the-little-schemer-clj/src/the_little_schemer_clj/chapter9.clj
+++ the-little-schemer-clj/src/the_little_schemer_clj/chapter9.clj
@@ -1,4 +1,3 @@
-
 (ns the-little-schemer-clj.chapter9
   (:use [the-little-schemer-clj.chapter2]
         [the-little-schemer-clj.chapter3]
@@ -32,3 +31,57 @@
          (build (second (first x)) (rest x))))
 
 (shift '(("a" "b") "c"))
+
+(defn align
+  [pora]
+  (cond
+    (atom? pora) pora
+    (a-pair? (first pora)) (align (shift pora))
+    :else (build (first pora)
+                 (align (second pora)))))
+
+;(align '("a"))
+
+(defn length*
+  [pora]
+  (cond
+    (atom? pora) 1
+    (empty? pora) 0
+    :else (+ (length* (first pora)) (length* (rest pora)))))
+
+;(length* '("a" ("b" "c") ("d" "g")))
+
+(defn weight*
+  [pora]
+  (cond
+    (atom? pora) 1
+    :else (+ (* (weight* (first pora)) 2) (weight* (second pora)))))
+
+;(weight* '(("a" "b") "c"))
+;(weight* '("a" ("b" "c")))
+
+(defn shuffle
+  [pora]
+  (cond
+    (atom? pora) pora
+    (a-pair? (first pora)) (shuffle (revpair pora))
+    :else (build (first pora)
+                 (shuffle (second pora)))))
+
+;(shuffle '(("a" "b") "c"))
+;(shuffle '("a" ("b" "c")))
+;(shuffle '("a" "b"))
+
+;(shuffle '(("a" "b") ("e" "d"))) ; this one cause a StackOverflowError => shuffle is not total
+
+(defn A
+  [n m]
+  (cond
+    (zero? n) (add1 m)
+    (zero? m) (A (sub1 n) 1)
+    :else (A (sub1 n) (A n (sub1 m)))))
+
+;(A 1 2)
+;(A 0 (A 1 1))
+;(A 0 3)
+;(A 4 3)
